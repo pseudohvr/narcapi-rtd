f1c9ad3352ba9af0ea3794ef0856bb634b1c0c36
pseudohvr <61255757+pseudohvr@users.noreply.github.com>
2023-04-11 14:20:29 -0500

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'Apollo AI | NARC API'
+copyright = '2023, Apollo AI'
+author = 'apollodev'
 
 release = '0.1'
 version = '0.1.0'
